ae524c69f640f13bd3fb730115fb0f43bd78f504
scyrety <mickeymed56@gmail.com>
2023-01-21 11:03:12 +0000

    Mark as read functionality

--- src/Pages/Notifications/Notifications.js
+++ src/Pages/Notifications/Notifications.js
@@ -4,6 +4,7 @@ import Notification from "./Notification/Notification";
 
 const Notifications = () => {
 	const [notifications, setNotifications] = useState([]);
+	const [waiting, setWaiting] = useState(false);
 	const { firebase, credentials } = useGlobalContext();
 
 	useEffect(() => {
@@ -16,25 +17,55 @@ const Notifications = () => {
 			// Group read and unread
 			res = [...res.filter((e) => e.status === "unread"), ...res.filter((e) => e.status === "read")];
 			if (res.error) return;
-			setNotifications(res);
+			setNotifications(res.length === 0 ? { empty: true } : res);
 		});
 	}, [firebase, credentials?.userId]);
+
+	function markRead() {
+		let unread = notifications.filter((e) => e.status === "unread");
+		if (unread.length) {
+			setWaiting(true);
+			unread.forEach((notification, index) => {
+				firebase.setReadNotification(notification.notification_id, credentials?.userId, (res) => {
+					if (res.error) return;
+					if (res && index === unread.length - 1) {
+						setTimeout(() => {
+							setWaiting(false);
+						}, 2000);
+					}
+				});
+			});
+		}
+	}
 	return (
 		<section className="notifications">
-			<div className="actions">
-				<div className="filter">
-					<i className="fa-solid fa-filter"></i>
-					<p>filter</p>
-				</div>
-				<button className="markRead">Mark As Read</button>
-			</div>
-			<div className="content">
-				{notifications?.empty && <p className="nothingHere">Nothing Here...</p>}
-				{!notifications?.empty &&
-					notifications.map((e) => {
-						return <Notification key={e.notification_id} {...e} receiver_id={credentials?.userId} />;
-					})}
-			</div>
+			{/* If notifications is empty. */}
+			{notifications?.empty && !waiting && <p className="nothingHere">Nothing Here...</p>}
+			{!notifications?.empty && (
+				<>
+					{/* If notifications is not empty and is not waiting for mark all as read functionality */}
+					{!waiting && (
+						<>
+							<div className="actions">
+								<div className="filter">
+									<i className="fa-solid fa-filter"></i>
+									<p>filter</p>
+								</div>
+								<button className="markRead" onClick={markRead}>
+									Mark As Read
+								</button>
+							</div>
+							<div className="content">
+								{notifications.map((e) => {
+									return <Notification key={e.notification_id} {...e} receiver_id={credentials?.userId} />;
+								})}
+							</div>
+						</>
+					)}
+					{/* If notifications is not empty but is  waiting for mark all as read functionality */}
+					{waiting && <p className="nothingHere waiting">Waiting...</p>}
+				</>
+			)}
 		</section>
 	);
 };

e6c667560475b5c932a4f4d9c1a8acfc71fb9be5
--global <--global>
2023-01-25 13:07:25 +0000

    Header , homepage and blog preview responsive

--- src/components/BlogPreview/BlogPreview.js
+++ src/components/BlogPreview/BlogPreview.js
@@ -50,60 +50,57 @@ const BlogPreview = ({ heading, message, blog_id, lead_image_src, dislikes, like
 	return (
 		<article className="blogPreview">
 			<div className="content">
-				<div className="left">
-					<NavLink to={link} className="link">
-						<h3 className="preview__heading">{truncateText(removeHTML(heading), 100)}</h3>
-					</NavLink>
-					<NavLink to={link} className="link">
-						<p>{truncateText(removeHTML(message), 195)} </p>
-					</NavLink>
-				</div>
 				<div className="leadImage">
 					<img src={lead_image_src} alt="Lead" />
 				</div>
-			</div>
-
-			<div className="actions">
-				<Ratings likes={likes} dislikes={dislikes} id={blog_id} type="blogs" />
-				<NavLink className="comments action" to={`${link}#comments`}>
-					<div className="icon">
-						<i className="fa-solid fa-comments"></i>
-					</div>
-					<p>{comments?.length}</p>
+				<NavLink to={link} className="link">
+					<h3 className="preview__heading">{truncateText(removeHTML(heading), 100)}</h3>
 				</NavLink>
-				<div className="views action">
-					<div className="icon">
-						<i className="fa-solid fa-eye"></i>
-					</div>
-					<p>{viewers ? viewers.length : 0}</p>
-				</div>
-
-				<p>${total}</p>
-
-				<Link className="username link" to={`/@${author}`}>
-					@{author}
-				</Link>
-
-				<article ref={subButton} className="blog_controls">
-					<i className="fa-solid fa-ellipsis-vertical"></i>
-					{sub && (
-						<div className="controls_sub">
-							{credentials?.user?.username === author && (
-								<NavLink className="elem" to={editLink}>
-									Edit article
-								</NavLink>
-							)}
-							<NavLink className="elem" to="/report">
-								Report this
-							</NavLink>
-							{credentials?.user?.username !== author && (
-								<NavLink className="elem" to={`/block/${userInfo?.userId}`}>
-									Block this user
-								</NavLink>
-							)}
+				<NavLink to={link} className="link">
+					<p>{truncateText(removeHTML(message), 195)} </p>
+				</NavLink>
+				<div className="actions">
+					<Ratings likes={likes} dislikes={dislikes} id={blog_id} type="blogs" />
+					<NavLink className="comments action" to={`${link}#comments`}>
+						<div className="icon">
+							<i className="fa-solid fa-comments"></i>
 						</div>
-					)}
-				</article>
+						<p>{comments?.length}</p>
+					</NavLink>
+					<div className="views action">
+						<div className="icon">
+							<i className="fa-solid fa-eye"></i>
+						</div>
+						<p>{viewers ? viewers.length : 0}</p>
+					</div>
+
+					<p>${total}</p>
+
+					<Link className="username link" to={`/@${author}`}>
+						@{author}
+					</Link>
+
+					<article ref={subButton} className="blog_controls">
+						<i className="fa-solid fa-ellipsis-vertical"></i>
+						{sub && (
+							<div className="controls_sub">
+								{credentials?.user?.username === author && (
+									<NavLink className="elem" to={editLink}>
+										Edit article
+									</NavLink>
+								)}
+								<NavLink className="elem" to="/report">
+									Report this
+								</NavLink>
+								{credentials?.user?.username !== author && (
+									<NavLink className="elem" to={`/block/${userInfo?.userId}`}>
+										Block this user
+									</NavLink>
+								)}
+							</div>
+						)}
+					</article>
+				</div>
 			</div>
 		</article>
 	);
